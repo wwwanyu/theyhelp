25801b4c2c6ce90077fae25375e34927adf65845
wwwanyu <2and3and4and1@gmail.com>
2025-10-14 14:04:07 +0800

    Add mobile menu

--- week1/js/app.js
+++ week1/js/app.js
@@ -1,1 +1,12 @@
-console.log('Hell world!');
+const mobileMenuButton = document.getElementById('Mobile_Menu_Button');
+const navItems = document.getElementById('Nav_Items');
+const MobileMenuClose = document.getElementById('Mobile_Menu_Close');
+
+mobileMenuButton.addEventListener('click', () => {
+    navItems.classList.add("d-block", "slideFromRight");
+});
+
+MobileMenuClose.addEventListener('click', () => {
+    navItems.classList.remove("d-block");
+    console.log('clicked')
+});
